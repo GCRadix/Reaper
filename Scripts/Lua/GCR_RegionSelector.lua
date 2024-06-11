bec0419d5876001f1e44de8a29e487f36bf73773
Fynthesizer <finn.mitchellanyon@gmail.com>
2024-06-11 12:59:48 +1200

    Clean up of region selector

--- Scripts/Lua/GCR_RegionSelector.lua
+++ Scripts/Lua/GCR_RegionSelector.lua
@@ -3,48 +3,38 @@
 
 local title = reaper.JS_Localize('Region/Marker Manager', 'common')
 
+package.path = reaper.GetResourcePath() .. '/Scripts/rtk/1/?.lua'
+local rtk = require('rtk')
+
 function GetAllRegions(lv)
   local cnt = reaper.JS_ListView_GetItemCount(lv)  
   local t = {}
   for i = 0, cnt-1 do
     if reaper.JS_ListView_GetItemText(lv, i, 1):match("R%d") then
-      t[#t+1] = i
-    end
-  end
-  return t
-end
-
-function GetSelectedRegions(lv)
-  local cnt = reaper.JS_ListView_GetItemCount(lv)  
-  local t = {}
-  for i = 0, cnt-1 do
-    local item_state = reaper.JS_ListView_GetItemState(lv, i)
-    if reaper.JS_ListView_GetItemText(lv, i, 1):match("R%d") then
-      if item_state == 3 or item_state == 2 then
-        t[#t+1]= i
-      end
+      local item_state = reaper.JS_ListView_GetItemState(lv, i)
+      local r = {
+        index=i, 
+        name=reaper.JS_ListView_GetItemText(lv, i, 2),
+        start_time = reaper.parse_timestr_pos(reaper.JS_ListView_GetItemText(lv, i, 3), 2),
+        end_time = reaper.parse_timestr_pos(reaper.JS_ListView_GetItemText(lv, i, 4), 2),
+        is_selected = (item_state == 3 or item_state == 2)
+      }
+      t[#t+1] = r
     end
   end
   return t
 end
 
 function GetNestedRegions(lv, regions)
-  local cnt = reaper.JS_ListView_GetItemCount(lv)  
   local t = {}
-  for i = 0, cnt-1 do
-    if reaper.JS_ListView_GetItemText(lv, i, 1):match("R%d") then
-      local region_start = reaper.parse_timestr_pos(reaper.JS_ListView_GetItemText(lv, i, 3), 2)
-      local region_end = reaper.parse_timestr_pos(reaper.JS_ListView_GetItemText(lv, i, 4), 2)
-      for j = 1, #regions do
-        -- exclude the region itself
-        if regions[j] ~= i then
-          -- check if the region is within a selected region
-          local sel_region_start = reaper.parse_timestr_pos(reaper.JS_ListView_GetItemText(lv, regions[j], 3), 2)
-          local sel_region_end = reaper.parse_timestr_pos(reaper.JS_ListView_GetItemText(lv, regions[j], 4), 2)
-          if region_start >= sel_region_start and region_end <= sel_region_end then
-            t[#t+1]= i
-            break
-          end
+  for i = 1, #regions do
+    for j = 1, #regions do
+      -- exclude the already selected region itself
+      if regions[j].is_selected and regions[j].index ~= regions[i].index then
+        -- check if the region is within this selected region
+        if regions[i].start_time >= regions[j].start_time and regions[i].end_time <= regions[j].end_time then
+          t[#t+1]= regions[i]
+          break
         end
       end
     end
@@ -52,21 +42,13 @@ function GetNestedRegions(lv, regions)
   return t
 end
 
-function TableContainsValue(table, value)
+function TableContainsRegion(table, region)
   for i = 1, #table do
-    if table[i] == value then 
+    if table[i].index == region.index then 
       return true
     end
   end
   return false
-end
-
-function LvSelectItem(hwnd, index, selected)
-  if selected then -- Select item @ index
-    reaper.JS_ListView_SetItemState(hwnd, index, 2, 2)
-  else -- Unselect item @ index
-    reaper.JS_ListView_SetItemState(hwnd, index, 1, -1)
-  end
 end
 
 function Main()
@@ -77,29 +59,10 @@ function Main()
     manager = reaper.JS_Window_Find(title, true) 
   end
 
-  retval, user_input_csv = reaper.GetUserInputs("Region selector", 4, 
-  "Nested within selected (Y/N),Containing text,Starting after,Ending before", 
-  "N,,,"
-  )
+  OpenUI()
+end
 
-  if retval == false then 
-    return
-  end
-
-  local user_input = {}
-  for value in string.gmatch(user_input_csv, '([^,]*),?') do
-    table.insert(user_input, value)
-  end
-
-  select_nested = user_input[1] == "Y" or user_input[1] == "y"
-  search_query = user_input[2]
-  after_time = user_input[3]
-  before_time = user_input[4] 
-
-  -- Fix in case of empty last value
-  if before_time == nil then
-    before_time = ''
-  end
+function PerformSelection(select_nested, search_query, before_time, after_time)
 
   local before_time_secs, after_time_secs
   if before_time ~= '' then
@@ -109,42 +72,81 @@ function Main()
     after_time_secs = reaper.parse_timestr_pos(after_time, 2)
   end
 
+  local manager = reaper.JS_Window_Find(title, true)
   if manager then
     reaper.DockWindowActivate(manager) -- OPTIONAL: Select/show manager if docked
     reaper.JS_Window_SetForeground(manager)-- Set focus on Manager window
     local lv = reaper.JS_Window_FindChild(manager, 'List2', true)
 
-    local region_indexes = GetAllRegions(lv)
-    local selected_region_indexes = GetSelectedRegions(lv)
-    local nested_region_indexes = GetNestedRegions(lv, selected_region_indexes)
+    local regions = GetAllRegions(lv)
+    local nested_regions = GetNestedRegions(lv, regions)
 
-    local sel_region = region_indexes[1]
-    
-    if sel_region then
-      for i = 1, #region_indexes do -- get current selection/focused region item
-        local should_select = false
-        local region_name = reaper.JS_ListView_GetItemText(lv, region_indexes[i], 2)
-        local region_start = reaper.parse_timestr_pos(reaper.JS_ListView_GetItemText(lv, region_indexes[i], 3), 2)
-        local region_end = reaper.parse_timestr_pos(reaper.JS_ListView_GetItemText(lv, region_indexes[i], 4), 2)
-        if select_nested == false or TableContainsValue(nested_region_indexes, region_indexes[i]) then
-          if search_query == '' or region_name.find(region_name, search_query) then
-              if before_time_secs == nil or region_end < before_time_secs then
-                  if after_time_secs == nil or region_start > after_time_secs then
-                      should_select = true
-                  end
-              end
-          end
+    for i = 1, #regions do -- get current selection/focused region item
+      local region = regions[i]
+      local should_select = false
+      if select_nested == false or TableContainsRegion(nested_regions, region) then
+        if search_query == '' or string.find(region.name, search_query) then
+            if before_time_secs == nil or region.end_time < before_time_secs then
+                if after_time_secs == nil or region.start_time > after_time_secs then
+                    should_select = true
+                end
+            end
         end
-        if should_select then
-            reaper.JS_ListView_SetItemState(lv, region_indexes[i], 2, 2) 
-        else
-            reaper.JS_ListView_SetItemState(lv, region_indexes[i], 1, -1) 
-        end
-      end 
-
-      reaper.JS_Window_SetFocus(lv)
+      end
+      if should_select then
+          reaper.JS_ListView_SetItemState(lv, region.index, 2, 2) 
+      else
+          reaper.JS_ListView_SetItemState(lv, region.index, 1, -1) 
+      end
     end 
+
+    reaper.JS_Window_SetFocus(lv)
   end 
+end
+
+function LogValue(value)
+  reaper.MB(value, "", 0)
+end
+
+function OpenUI()
+  local selector = rtk.Window{w=300, h=300, title="Region Selector"}
+
+  local box_main = rtk.VBox{fillw=true, margin=20, w=1.0}
+  local cb_select_nested = rtk.CheckBox{'Nested within selected regions', bmargin=20, w=1.0}
+  local entry_search_query = rtk.Entry{placeholder='Name containing text', textwidth=15, bmargin=20, w=1.0}
+  local entry_after_time = rtk.Entry{placeholder='Starting after (0.0.00)', textwidth=15, bmargin=20, w=1.0}
+  local entry_before_time = rtk.Entry{placeholder='Ending before (0.0.00)', textwidth=15, bmargin=20, w=1.0}
+
+  local box_buttons = rtk.HBox()
+  local btn_submit = rtk.Button{label='Run', rmargin=10, color="green", w=80}
+  btn_submit.onclick = function()
+    PerformSelection(cb_select_nested.value, entry_search_query.value, entry_after_time.value, entry_before_time.value)
+    selector:close()
+  end
+  local btn_cancel = rtk.Button{label='Close', w=80}
+  btn_cancel.onclick = function()
+    selector:close()
+  end
+  box_buttons:add(btn_submit)
+  box_buttons:add(btn_cancel)
+
+  box_main:add(cb_select_nested)
+  box_main:add(entry_search_query)
+  box_main:add(entry_after_time)
+  box_main:add(entry_before_time)
+  box_main:add(box_buttons)
+
+  selector:add(box_main)
+  
+  selector.onkeypresspost = function(self, event)
+    if not event.handled and event.keycode == rtk.keycodes.ENTER and
+       not selector.docked then
+        PerformSelection(cb_select_nested.value, entry_search_query.value, entry_after_time.value, entry_before_time.value)
+        selector:close()
+    end
+  end
+
+  selector:open{align='center'}
 end
 
 if not reaper.APIExists('JS_Localize') then
